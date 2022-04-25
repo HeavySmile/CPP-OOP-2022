82b7121187d029eb6b6597f0f11a5bcc9d49c8f2
HeavySmile <lenyashubin2002@gmail.com>
2022-04-25 10:53:16 +0300

    Initial commit

--- Matrix/Main.cpp
+++ Matrix/Main.cpp
@@ -6,11 +6,8 @@ int main()
 {
 	Matrix matrix1(3, 3);
 	matrix1.readMatrix();
-	//matrix1 = 2 * matrix1;
+	matrix1 = 2 * matrix1;
 	matrix1.printMatrix();
 	cout << matrix1.isInvertable() << endl;
 	cout << matrix1.findDeterminant() << endl;
-	//matrix1.transponse();
-	//matrix1.printMatrix();
 }
-
